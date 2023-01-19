2820234a62f366442c1bde8627d8bd28df0bc594
Ksenia Yurko <iurko.kk@phystech.edu>
2023-01-19 03:21:08 +0300

    vector5 done

--- Third semestr Cpp/vector/vector5.cpp
+++ Third semestr Cpp/vector/vector5.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 #include <vector> 
-#include "base_functions.h"
+#include <string>
 #include <algorithm>
 
 using namespace std;
 
+int print_vector(vector <string> &vec){
+        for(int i = 0; i < vec.size(); i++){
+            cout << vec[i] << " ";
+        }
+        cout << "\n";
+    return 0;
+
+}
+
 int main(){
-    vector <int> vec;
-    for(int i = 0; )
+    vector <string> vec;
+    int N = vec.size();
+    
+    for(int i = 0; i < 3; i++){
+        string letter;
+        cin >> letter;
+        vec.push_back(letter);  
+    }
+
+    print_vector(vec);
+    sort(vec.begin(), vec.end());
+    
+    int o = 0;
+    for(int i = 0; i < vec[0].size(); i++){
+        if(vec[0][i] == 'o'){
+            o++;
+        }
+    }
+
+    print_vector(vec);
+    cout << o;
 }
