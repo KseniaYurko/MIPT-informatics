66aa09ae23602d97a86b6cab47884350a8de4398
Ksenia Yurko <iurko.kk@phystech.edu>
2023-02-08 07:45:36 +0300

    relise -, fix reducton

--- Fourth semestr/Operator overloading/1_Class_Rational.cpp
+++ Fourth semestr/Operator overloading/1_Class_Rational.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 void reduction(int &a, int &b){
     int i = 2;
-    while(i <= b && i <= a){
+    while(i <= b && i <= abs(a)){
         if(b % i == 0 && a % i == 0){
             b = b / i;
             a = a / i;
@@ -28,58 +28,81 @@ void change_sign(int &a, int &b){
 
 class rational {
     public:
-        int numerator;
-        int denominator;
+        int num;
+        int denom;
 
     rational(){
-        numerator = 0;
-        denominator = 1;
+        num = 0;
+        denom = 1;
     }
 
-    rational(int numerator_, int denominator_){
-        numerator = numerator_;
-        denominator = denominator_; 
+    rational(int num_, int denom_){
+        num = num_;
+        denom = denom_; 
 
-        if(denominator < 0){
-            change_sign(numerator, denominator);
+        if(denom < 0){
+            change_sign(num, denom);
         }
-        else if(denominator = 0){
-            throw invalid_argument("denominator = 0");
+        else if(denom == 0){
+            throw invalid_argument("denom = 0");
         }  
-        else denominator = denominator_;
-
-        reduction(numerator, denominator);
-        // cout << numerator << " " << denominator << " - reduction" << endl;
+        reduction(num, denom);
     }
 
     //sum
     rational operator+ (rational fr2){
-        numerator = numerator * fr2.denominator + fr2.numerator * denominator; 
-        denominator = denominator * fr2.denominator;
-        reduction(numerator, denominator); 
-        return *this;
+        fr2.num = num * fr2.denom + fr2.num * denom; 
+        fr2.denom = denom * fr2.denom;
+        reduction(fr2.num, fr2.denom); 
+        return fr2;
     }
 
     //substraction
     rational operator- (rational fr2){
-        numerator = numerator * fr2.denominator - fr2.numerator * denominator; 
-        denominator = denominator * fr2.denominator;
-        reduction(numerator, denominator);
-        return *this;
+        rational fr3;
+        fr3.num = num * fr2.denom - fr2.num * denom;
+        fr3.denom = denom * fr2.denom;
+        reduction(fr3.num, fr3.denom);
+        return fr3;
+    }
+
+    //multiply
+    rational operator* (rational fr2){
+        fr2.num = num * fr2.num; 
+        fr2.denom = denom * fr2.denom;
+        reduction(fr2.num, fr2.denom);
+        return fr2;
+    }
+
+    //division
+    rational operator/ (rational fr2){
+        rational fr3;
+        fr3.denom = fr2.denom;
+        fr3.num = fr2.num;
+
+        fr3.num = num * fr2.denom; 
+        fr3.denom = denom * fr2.num;
+        reduction(fr3.num, fr3.denom);
+        return fr3;
     }
 };
 
 //display class
 ostream& operator<< (ostream& os, rational fr){
-    return os << fr.numerator << "/" << fr.denominator << endl;
+    return os << fr.num << "/" << fr.denom << endl;
 };
 
 
 int main(){
-    rational fr1(1, 2);
-    rational fr2(2, -4);
+    rational fr1(7, 14);
+    rational fr2(13, 14);
+
+    rational r1(6,15);
 
     cout << fr1 << endl;
     cout << fr2 << endl;
-    cout << fr1 - fr2 << endl;
+    cout << fr1 + fr2 << " - sum" << endl;
+    cout << fr1 - fr2 << " - substruction" << endl;
+    cout << fr1 * fr2 << " - multiply" << endl;
+    cout << fr1 / fr2 << " - divsion" << endl;
 }
