c118ee294b250cb916282035fc48fda9a50b14ba
Ksenia Yurko <iurko.kk@phystech.edu>
2023-03-23 02:25:10 +0300

    binary tree

--- Trees/binary_tree.cpp
+++ Trees/binary_tree.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+#include <vector>
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <algorithm>
+#include <tuple>
+#include <stdexcept>
+#include <memory>
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <locale.h>
+#endif
 
 using namespace std;
 
@@ -53,6 +68,53 @@ void print_tree(tree_elem* cur_elem){
     
 }
 
+//DISPLAY TREE ------------------------------------------------------------------------------------------------------------------//
+#define LN { std::cout << __LINE__ << std::endl; }
+#define DEB(x) { std::cout << #x << "=(" << (x) << ") "; }
+static std::string ch_hor = "\u2500" /*─*/, ch_ver = "\u2502" /*│*/, ch_ddia = "\u250C" /*┌*/, ch_rddia = "\u2510" /*┐*/, ch_udia = "\u2514" /*└*/, ch_ver_hor = "\u251C\u2500" /*├─*/, ch_udia_hor = "\u2514\u2500" /*└─*/, ch_ddia_hor = "\u250C\u2500" /*┌─*/, ch_ver_spa = "\u2502 " /*│ */;
+
+void dump4(tree_elem const * node, bool high = true, 
+            vector<string> const & lpref = vector<string>(),
+            vector<string> const & cpref = vector<string>(), 
+            vector<string> const & rpref = vector<string>(), 
+            bool root = true, 
+            bool left = true, 
+            shared_ptr<vector<vector<string>>> lines = nullptr){
+    
+    if (!node) return;
+
+    typedef vector<string> VS;
+
+    auto VSCat = [](VS const & a, VS const & b){ auto r = a; r.insert(r.end(), b.begin(), b.end()); return r; };
+
+    if (root) lines = make_shared<vector<VS>>();
+    if (node->left);
+
+    dump4(node->left, high, VSCat(lpref, high ? VS({" ", " "}): VS({" "})), VSCat(lpref, high ? VS({ch_ddia, ch_ver}) : VS({ch_ddia})), VSCat(lpref, high ? VS({ch_hor, " "}) : VS({ch_hor})), false, true, lines);
+    auto sval = to_string(node->value);
+    size_t sm = left || sval.empty() ? sval.size() / 2 : ((sval.size() + 1) / 2 - 1);
+    for (size_t i = 0; i < sval.size(); ++i)
+        lines->push_back(VSCat(i < sm ? lpref : i == sm ? cpref : rpref, {string(1, sval[i])}));
+    if (node->right)
+        dump4(node->right, high, VSCat(rpref, high ? VS({ch_hor, " "}) : VS({ch_hor})), VSCat(rpref, high ? VS({ch_rddia, ch_ver}) : VS({ch_rddia})), VSCat(rpref, high ? VS({" ", " "}) : VS({" "})), false, false, lines);
+    if (root) {
+        VS out;
+        for (size_t l = 0;;++l) {
+            bool last = true;
+            string line;
+            for (size_t i = 0; i < lines->size(); ++i)
+                if (l < (*lines).at(i).size()) {
+                    line += lines->at(i)[l];
+                    last = false;
+                } else line += " ";
+            if (last) break;
+            out.push_back(line);
+        }
+        for (size_t i = 0; i < out.size(); ++i)
+            cout << out[i] << endl;
+    }
+}
+//-------------------------------------------------------------------------------------------------------------------------------//
 
 
 int main(){
@@ -65,7 +127,7 @@ int main(){
     }
 
     cout << "tree: " << endl;
-    print_tree(root);
+    dump4(root, true);
 
 
 
